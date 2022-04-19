c1f4e4e92f57e654185aa64fdc3db33fe347fd1d
NicolasPlanas1998 <nicolasplanas1998@gmail.com>
2022-04-18 21:14:34 -0300

    Navbar responsive behavior

---
+++ js/main.js
@@ -0,0 +1,10 @@
+hambur = document.querySelector('.fa-bars')
+menu = document.querySelector('.first-menu')
+
+hambur.addEventListener('click',dropdown)
+
+function dropdown (e) {
+    e.preventDefault(e)
+    menu.classList.toggle('expand')
+
+}
