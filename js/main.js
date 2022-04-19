30294b2ef07f41b21d1bfe8d214c12c54d64ddad
NicolasPlanas1998 <nicolasplanas1998@gmail.com>
2022-04-19 15:21:55 -0300

    Save form data

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
-hambur = document.querySelector('.fa-bars')
-menu = document.querySelector('.first-menu')
+"use strict"
+//  menu responsive
+let hambur = document.querySelector('.fa-bars')
+let menu = document.querySelector('.first-menu')
 
 hambur.addEventListener('click',dropdown)
 
 function dropdown (e) {
-    e.preventDefault(e)
+    e.preventDefault()
     menu.classList.toggle('expand')
     hambur.classList.toggle('rotate')
+}
+
+// informacion formulario
+
+let nombre = document.getElementsByClassName("inputs__contacto")
+
+let form_btn = document.querySelector(".boton__config--cliente")
+
+form_btn.addEventListener("click", getInfo)
+
+function getInfo(e){
+    e.preventDefault()
+    let informacion = []
+    for(let i=0; i <= 3 ; i++){
+            informacion.push(nombre[i].name, nombre[i].value)
+            }
+    localStorage.setItem('usuario', JSON.stringify(informacion))
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Tu consulta fue enviada',
+        showConfirmButton: false,
+        timer: 1500
+    })
 }
